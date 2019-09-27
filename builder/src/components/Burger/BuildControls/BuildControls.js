511584694aae22a25f83f19d369176f7358f5ec0
Mayankho <yankhot96@gmail.com>
2019-09-26 23:49:40 -0400

    Build controls 120 reverse

--- builder/src/components/Burger/BuildControls/BuildControls.js
+++ builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+
+
 
 const buildControls = (props) => (
-    <div>
-        
-
+    <div className={classes.BuildControls}>
+        {buildControls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
 );
 
