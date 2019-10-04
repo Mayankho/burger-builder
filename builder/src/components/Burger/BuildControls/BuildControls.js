86a0dc54677f362261c3db6cca02509a86afd00e
Mayankho <yankhot96@gmail.com>
2019-10-03 23:56:03 -0400

    add remove

--- builder/src/components/Burger/BuildControls/BuildControls.js
+++ builder/src/components/Burger/BuildControls/BuildControls.js
@@ -18,8 +18,14 @@ const buildControls = (props) => (
             <BuildControl 
             key={ctrl.label} 
             label={ctrl.label}
-            added = {() => props.ingredientAdded(ctrl.type)} />
+            added = {() => props.ingredientAdded(ctrl.type)}
+            removed = {() => props.ingredientRemoved(ctrl.type)}
+            
+            />
         ))}
+        <button>
+                ORDER NOW
+        </button>
     </div>
 );
 
