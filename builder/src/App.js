e58fafd48f9499ce941b669714f6b6bd167cddf5
Mayankho <yankhot96@gmail.com>
2019-10-22 23:57:47 -0400

    bridge

--- builder/src/App.js
+++ builder/src/App.js
@@ -10,9 +10,10 @@ class App extends Component {
             <BurgerBuilder />
         </Layout>
       </div>
-
+      
       //The layout method is another way of awrapping the aapp completly in the root componinet
       //I will s get naeter at a typooing and in every abven e of the soul that is a giveing o en vwint heeven hd
+
     );
   }
 }
