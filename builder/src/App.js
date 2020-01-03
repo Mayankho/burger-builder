b6c652ae3fe5823f908f790d6e757f70692e767f
Mayankho <yankhot96@gmail.com>
2020-01-02 23:00:26 -0500

    re-configured App

--- builder/src/App.js
+++ builder/src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/layout';
+
+import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App"> 
+      <div>
         <Layout>
-            <BurgerBuilder />
+          <BurgerBuilder />
         </Layout>
       </div>
-      
-      //The layout method is another way of awrapping the aapp completly in the root componinet
-      //I will s get naeter at a typooing and in every abven e of the soul that is a giveing o en vwint heeven hd
-
     );
   }
 }
-
-
 
 export default App;
