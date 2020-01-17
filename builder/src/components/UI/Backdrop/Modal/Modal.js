f0272d13982b017e811a141a88b35e942ea7ffca
Mayankho <yankhot96@gmail.com>
2020-01-16 23:30:27 -0500

    Added Modal and Backdrop

---
+++ builder/src/components/UI/Backdrop/Modal/Modal.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import classes from './Modal.css'
+
+ 
+const modal = (props) => ( 
+    <div className = {classes.Modale}>
+        {props.children}
+    </div>
+);
+
+
+export default modal;
