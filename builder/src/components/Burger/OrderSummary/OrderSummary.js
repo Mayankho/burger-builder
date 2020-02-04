d6db7cc620c6ac45b1595602a0f9f92d002b39d9
Mayankho <yankhot96@gmail.com>
2020-02-03 23:41:06 -0500

    buffer

---
+++ builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+        return 
+    } );
+    <lli>Salad: 1</lli>
+    return(
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious</p>
+            <ul>
+
+            </ul>
+        </Aux>
+    )
+};
+//Buffer
+export default orderSummary;
